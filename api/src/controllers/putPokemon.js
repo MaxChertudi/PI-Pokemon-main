f609152ed79562544ed054ceb32451f78a0dcc9c
MaxChertudi <126177692+MaxChertudi@users.noreply.github.com>
2023-06-02 14:16:01 -0300

    02

--- api/src/controllers/putPokemon.js
+++ api/src/controllers/putPokemon.js
@@ -3,11 +3,9 @@ const postPokemon = require('./postPokemon');
 const axios = require('axios');
 
 const putPokemon = async(req, res) => {
-    //await deletePokemonByid(req, res);
-    //await postPokemon(req, res);
     try {
-        //const aDelete = await axios.delete('http://localhost:3001/pokemons/' + req.body.id );
-        //const aPost = await axios.post('http://localhost:3001/pokemons', req.body);
+        await deletePokemonByid(req, res);
+        await postPokemon(req, res);
         return res.status(200);
     } catch(error) {
         return res.status(502).send('Error trying to update pokemon: ', error.message);
