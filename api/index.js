1c461e2a7745122205a8bf299befe42426d7f146
MaxChertudi <126177692+MaxChertudi@users.noreply.github.com>
2023-06-15 16:40:25 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
